b5b20c0ff3be466e61fd94c47be1201d64375b53
Muhammad Junaid <junaid1460@gmail.com>
2019-08-12 14:07:42 +0530

    remove semicolon

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,9 @@
 export declare class PxEnforcer{
     public enforce<T, F>(req:F, res: T, cb: (error:Error, response:  T) => void);
     constructor(params: any, client: PxClient)
-    config: any
-};
+    public config: any
+}
+
 export declare class PxClient{
 
 }
