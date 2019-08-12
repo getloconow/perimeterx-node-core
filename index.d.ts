efe1831e365e71ee327b02f8b1b6aea03401f060
Muhammad Junaid <junaid1460@gmail.com>
2019-08-12 13:29:37 +0530

    Add config var

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
 export declare class PxEnforcer{
     public enforce<T, F>(req:F, res: T, cb: (error:Error, response:  T) => void);
     constructor(params: any, client: PxClient)
+    config: any
 };
 export declare class PxClient{
 
