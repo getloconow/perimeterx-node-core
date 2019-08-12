13253000e49bd4b6e3dc10652870d2ed9f7100ab
Muhammad Junaid <junaid1460@gmail.com>
2019-08-12 13:27:16 +0530

    Accept PX client

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,6 @@
 export declare class PxEnforcer{
     public enforce<T, F>(req:F, res: T, cb: (error:Error, response:  T) => void);
+    constructor(params: any, client: PxClient)
 };
 export declare class PxClient{
 
